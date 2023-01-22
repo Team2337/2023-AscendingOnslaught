a689ce13f605ffee0b6c1b7dd5ba2692babd0817
MadisonJohnston <madison.johnston333@gmail.com>
2023-01-22 14:49:39 -0500

    Added auto chooser for red and blue starting positions

--- src/main/java/frc/robot/commands/vision/PeriodicRelocalizeCartesian.java
+++ src/main/java/frc/robot/commands/vision/PeriodicRelocalizeCartesian.java
@@ -5,21 +5,13 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.Constants;
-import frc.robot.coordinates.PolarCoordinate;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Vision;
 
 public class PeriodicRelocalizeCartesian extends VisionCommand {
 
-  // The most recent Vision pose - stored for our scope visualization
-  private Pose2d pose;
-  private Field2d field = new Field2d();
-
-  // Feed our pose estimator a new vision pose every ~0.2s
-  private static final int RELOCALIZE_DEBOUNCE_LIMIT = 10;
-  private int relocalizeDebounceCounter = 0;
+  private boolean disableRelocalizationInAuto = true;
 
   public PeriodicRelocalizeCartesian(Drivetrain drivetrain, Vision vision) {
     this.vision = vision;
@@ -30,6 +22,10 @@ public class PeriodicRelocalizeCartesian extends VisionCommand {
 
   @Override
   public void execute() {
+    //TODO: When do we turn this off?
+    if (DriverStation.isAutonomous() && disableRelocalizationInAuto) {
+      return;
+    }
     if (vision.getVisionPose().length != 0) {
 
       double visionPoseX = vision.getVisionPoseX();
@@ -42,9 +38,6 @@ public class PeriodicRelocalizeCartesian extends VisionCommand {
       );
 
       drivetrain.addVisionMeasurement(pose, Timer.getFPGATimestamp() - ((Constants.Vision.IMAGE_PROCESSING_LATENCY_MS + vision.getLatency() + 2) / 1000));
-      // drivetrain.resetPosition(pose);
-      // vision.incrementRelocalizeCounter();
-      
     } 
   }
 }
