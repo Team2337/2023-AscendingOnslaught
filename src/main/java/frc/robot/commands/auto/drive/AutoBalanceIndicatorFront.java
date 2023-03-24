e3792cb9872233cafebb923e87b40e9f38dbcd2d
Sean Lynch <slynch05@yahoo.com>
2023-03-23 23:03:28 -0400

    Balance commands

---
+++ src/main/java/frc/robot/commands/auto/drive/AutoBalanceIndicatorFront.java
@@ -0,0 +1,41 @@
+package frc.robot.commands.auto.drive;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class AutoBalanceIndicatorFront extends CommandBase {
+    
+    private Supplier<Rotation2d> pitchSupplier;
+    private boolean isElevated = false; 
+
+
+    public AutoBalanceIndicatorFront(Supplier<Rotation2d> pitchSupplier) {
+        this.pitchSupplier = pitchSupplier;
+    }
+
+    @Override
+    public void initialize() {
+    }
+  
+    @Override
+    public void execute() {
+  
+      if (pitchSupplier.get().getDegrees() < -12) {
+        isElevated = true;
+      }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+      end(interrupted);
+  
+    }
+  
+    @Override
+    public boolean isFinished() {
+      return (isElevated && pitchSupplier.get().getDegrees() > -9.1);
+    }
+    
+}
